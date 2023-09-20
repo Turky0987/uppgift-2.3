c322e013edc41c9abda5434fe6eecb2fb4490bbd
taal04003 <taal04003@E-PW0234KH>
2023-09-20 09:53:05 +0200

    Add project files.

---
+++ uppgift 2.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarRentalCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int startFee = 300;
+            const int costPerKilometer = 1;
+            const int extraDayFee = 500;
+
+            Console.WriteLine("Välkommen till biluthyrningsföretaget!");
+            Console.Write("Hur många dagar vill du hyra bilen? ");
+            int rentalDays = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur många kilometer vill du köra? ");
+            int kilometersDriven = int.Parse(Console.ReadLine());
+
+            int totalCost = startFee + (rentalDays - 1) * extraDayFee + kilometersDriven * costPerKilometer;
+
+            Console.WriteLine($"Total hyra för {rentalDays} dagar och {kilometersDriven} kilometer är: {totalCost} kr");
+        }
+    }
+}
